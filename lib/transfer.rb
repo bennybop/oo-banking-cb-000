55b3399b93afca60416d25ae8105d315608981fb
bennybop <bennybop74@yahoo.com>
2018-01-14 20:15:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,40 +1,40 @@
 class Transfer
-  attr_reader :amount, :sender, :receiver 
-  attr_accessor :status 
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
 
   def initialize(sender, receiver, amount)
     @status = "pending"
-    @sender = sender 
-    @receiver = receiver 
-    @amount = amount 
-end
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
 
-def valid? 
+  def valid?
   sender.valid? && receiver.valid?
-end 
+  end
 
-def execute_transaction 
-  if valid? && sender.balance > amount && self.status == "pending"
-    sender.balance -= amount 
-    receiver.balance += amount 
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+    sender.balance -= amount
+    receiver.balance += amount
     self.status = "complete"
-  else 
+    else
     reject_transfer
-  end 
-end 
+    end
+  end
 
-def reverse_transfer 
-  if valid? && receiver.balance > amount && self.status == "complete"
-    receiver.balance -= amount 
-    sender.balance += amount 
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+    receiver.balance -= amount
+    sender.balance += amount
     self.status = "reversed"
-  else 
+    else
     reject_transfer
-  end 
-end 
+    end
+  end
 
-def reject_transfer
-  self.status = "rejected"
-  "Transaction rejected. Please check your account balance."
-end
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end 
